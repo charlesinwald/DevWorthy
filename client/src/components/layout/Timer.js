25f46c2f6002ae59ea2191b95ce67d3511d66126
Nathan Demmel <nyd221@lehigh.edu>
2020-11-11 16:50:37 -0800

    work on localstorage and set limit in timer

--- client/src/components/layout/Timer.js
+++ client/src/components/layout/Timer.js
@@ -3,35 +3,41 @@ import moment from "moment";
 
 const Timer = () => {
   //TODO load in from local storage if it exists, if not set to zero
-  if(localStorage.getItem('elapsedTime') == null) {
-    localStorage.setItem('elapsedTime', "0");
+  const today = moment();
+  if(localStorage.getItem(today.format("DDD YYYY")) == null) {
+    localStorage.setItem(today.format("DDD YYYY"), "0");
   }
-  const [seconds, setSeconds] = useState(parseInt(localStorage.getItem('elapsedTime')));
+  const [seconds, setSeconds] = useState(parseInt(localStorage.getItem(today.format("DDD YYYY"))));
   const [isActive, setIsActive] = useState(true);
-
+  localStorage.setItem('alertTime', "60");
   function toggle() {
     setIsActive(!isActive);
   }
 
   window.onbeforeunload = function() {
-    localStorage.setItem('elapsedTime', seconds.toString());
+    localStorage.setItem(today.format("DDD YYYY"), seconds.toString());
   }
-  
+
   // function reset() {
   //   setSeconds(0);
   //   setIsActive(false);
   // }
-
+  
+  // constantly running to increment the timer
   useEffect(() => {
     let interval = null;
+    // if the timer is active(which it always is) increment the seconds count every second
     if (isActive) {
       interval = setInterval(() => {
         setSeconds(seconds => seconds + 1);
       }, 1000);
-    } else if (!isActive && seconds !== 0) {
-      clearInterval(interval);
+    }
+    if (seconds == parseInt(localStorage.getItem('alertTime'))){
+      alert("timer reached");
+      console.log("timer reached");
     }
     return () => clearInterval(interval);
+    
   }, [isActive, seconds]);
 
   const prettyTime = moment.utc(seconds*1000).format('mm:ss');
@@ -39,11 +45,19 @@ const Timer = () => {
   return (
     <div className="timer">
       <div className="time">
-        {seconds}
-        {/* {prettyTime} */}
+        {prettyTime} 
       </div>
       <div>
-
+      <TextField
+            //className={classes.titlearea}
+            autoComplete='off'
+            id="setLimit"
+            // So we can retrieve value
+            inputRef={Timer.textInput}
+            label="Time Limit"
+            placeholder="30:00"
+            variant="outlined"
+        />
       </div>
     </div>
   );
